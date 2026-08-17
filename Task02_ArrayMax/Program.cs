328605a13749075c11ba8704b7850c6d2ee36643
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 01:58:48 +0400

    Add task 2 array max

---
+++ Task02_ArrayMax/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { 34, 56, 32,78,4};
+int maximum = numbers[0];
+for(int i = 0;i< numbers.Length; i++)
+{
+    if(numbers[i]> maximum)
+    {
+        maximum = numbers[i];
+    }
+}
+Console.WriteLine($"Max: {maximum}");
