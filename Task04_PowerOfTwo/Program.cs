524a2d4e5ad557ece10c815da081cc016937a749
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 14:06:21 +0400

    Complete task 4 power of two

--- Task04_PowerOfTwo/Program.cs
+++ Task04_PowerOfTwo/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 56;
+bool isQuvveti = true; 
+while (n > 1)
+{
+    if(n%2 == 1)
+    {
+        isQuvveti = false;
+        break;
+    }
+    n/=2; //4 2 1
+}
+if (isQuvveti == true)
+{
+    Console.WriteLine("Quvvetidir");
+
+} else
+    Console.WriteLine("Quvveti deyil");
