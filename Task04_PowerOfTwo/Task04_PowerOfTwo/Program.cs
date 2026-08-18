f86f94ae94e14b360a60076069bd15799ea73d36
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 13:53:34 +0400

    Add task 4

---
+++ Task04_PowerOfTwo/Task04_PowerOfTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 56;
+bool isQuvveti = true; 
+while (n > 1)
+{
+    if(n%2 == 1)
+    {
+        isQuvveti = false;
+        break;
+    }
+    n/=2; //4 2 1
+}
+if (isQuvveti == true)
+{
+    Console.WriteLine("Quvvetidir");
+
+} else
+    Console.WriteLine("Quvveti deyil");
