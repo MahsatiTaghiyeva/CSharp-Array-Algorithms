1638444cf73d511b64f8feeec9516c4e69fa1cc0
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 14:50:37 +0400

    Add second Task6 Encreamenting positive nums

---
+++ Task06_IncrementNonNegative/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] nums = {1, 23, -4, 456,-7};
+for(int i = 0; i< nums.Length; i++)
+{
+    if (nums[i] >= 0)
+    {
+        nums[i]+= 2;
+    }
+}
+foreach(int num in nums)
+{
+    Console.WriteLine(num);
+}
