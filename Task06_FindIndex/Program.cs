95ae85eaaf8481c9cf6a77170a774d4584e68b0f
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 14:35:26 +0400

    Add Task06

---
+++ Task06_FindIndex/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = { 21, 34, 48, 67, 91 };
+bool exists50 = false;
+for(int i = 0; i < nums.Length; i++)
+{
+    if(nums[i]== 50)
+    {
+        exists50 = true;
+        Console.WriteLine(i);
+        break;
+    }
+}
+if(exists50 == false)
+{
+    Console.WriteLine(-1);
+}
