1afc88038353af7e72e6553e4848dd2abca77dbd
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 16:25:17 +0400

    Add Task 8

---
+++ Task09_MinArrayElement/Task09_MinArrayElement/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] nums = [12, 3, 87, 7, 6, 12];
+int min = nums[0];
+for(int i =0; i<nums.Length; i++)
+{
+    if(nums[i] < min)
+    {
+        min = nums[i];
+    }
+}
+Console.WriteLine($"Min: {min}");
