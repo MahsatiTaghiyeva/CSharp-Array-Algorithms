4208a8f196f03c7afc58c4c5aa62d80f2e4b2510
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 16:08:30 +0400

    Add task 7

---
+++ Task07_CountOccurrences/Task07_CountOccurrences/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] nums = [1, 2, 3, 2, 4, 2];
+int count = 0;
+int find = 2;
+foreach(int num in nums){
+    if(num == find)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Count of {find}: {count}");
